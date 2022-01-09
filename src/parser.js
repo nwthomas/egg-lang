cb7fbc604caa9cebc7e91f2429a01f89a9be0ccf
Nathan Thomas <nwthomas@me.com>
2022-01-09 12:42:10 -0800

    Add code comments from walkthrough of parser.js file

--- src/parser.js
+++ src/parser.js
@@ -2,10 +2,12 @@
 function parse(program) {
   const { expr, rest } = parseExpression(program);
 
+  // Handle bad syntax with unfinished expression
   if (skipSpace(rest).length > 0) {
     throw new SyntaxError("Unexpected text after program");
   }
 
+  // Return total parsed expression of program
   return expr;
 }
 
@@ -14,46 +16,75 @@ function parseExpression(newProgram) {
   const normalizedProgram = skipSpace(newProgram);
   let match, expr;
 
+  // Case for handling strings
   if ((match = /^"([^"]*)"/.exec(normalizedProgram))) {
     expr = { type: "value", value: match[1] };
-  } else if ((match = /^\d+\b/.exec(normalizedProgram))) {
+  }
+
+  // Case for handling numbers
+  else if ((match = /^\d+\b/.exec(normalizedProgram))) {
     expr = { type: "value", value: Number(match[0]) };
-  } else if ((match = /^[^\s(),#"]+/.exec(normalizedProgram))) {
+  }
+
+  // Case for handling words
+  else if ((match = /^[^\s(),#"]+/.exec(normalizedProgram))) {
     expr = { type: "word", name: match[0] };
-  } else {
+  }
+
+  // Case for when the above aren't true - throw SyntaxError
+  else {
     throw new SyntaxError("Unexpected syntax: " + normalizedProgram);
   }
 
+  // If one of the happy cases, run parseApply()
   return parseApply(expr, normalizedProgram.slice(match[0].length));
 }
 
-function parseApply(expr, program) {
-  program = skipSpace(program);
-  if (program[0] != "(") {
-    return { expr: expr, rest: program };
+// Allows the parsing of a parenthesized list of arguments if expression
+// is not an application
+function parseApply(expr, newProgram) {
+  let normalizedProgram = skipSpace(newProgram);
+
+  // Handle exit case where expression is not start of new application
+  // and preserve existing expression from parent function call
+  if (normalizedProgram[0] != "(") {
+    return { expr: expr, rest: normalizedProgram };
   }
 
-  program = skipSpace(program.slice(1));
+  // Slice off opening parenthesis
+  normalizedProgram = skipSpace(normalizedProgram.slice(1));
   expr = { type: "apply", operator: expr, args: [] };
 
-  while (program[0] != ")") {
-    let arg = parseExpression(program);
+  // Loop through application until ending parenthesis is hit
+  while (normalizedProgram[0] != ")") {
+    // Recursively parse existing program using this same process
+    const arg = parseExpression(normalizedProgram);
+
     expr.args.push(arg.expr);
-    program = skipSpace(arg.rest);
-    if (program[0] == ",") {
-      program = skipSpace(program.slice(1));
-    } else if (program[0] != ")") {
+    normalizedProgram = skipSpace(arg.rest);
+
+    // Slice off next argument ", " or ","
+    if (normalizedProgram[0] == ",") {
+      normalizedProgram = skipSpace(normalizedProgram.slice(1));
+    }
+
+    // Else, throw new SyntaxErro
+    else if (normalizedProgram[0] != ")") {
       throw new SyntaxError("Expected ',' or ')'");
     }
   }
-  return parseApply(expr, program.slice(1));
+
+  // Recursively call itself
+  return parseApply(expr, normalizedProgram.slice(1));
 }
 
 // Removes any initial spaces from the front of an expression
 function skipSpace(string) {
+  // Find first occurance of " " char
   const first = string.search(/\S/);
 
+  // Return sliced off space char or else empty string
   return first == -1 ? "" : string.slice(first);
 }
 
-console.log(parse("+(a, 10)"));
+module.exports = parse;
---
+++ src/run.js
@@ -0,0 +1,18 @@
+const evaluate = require("./evaluator.js");
+const parse = require("./parser.js");
+const topScope = require("./environment.js");
+
+function run(program) {
+  return evaluate(parse(program), Object.create(topScope));
+}
+
+run(`
+do(define(total, 0),
+   define(count, 1),
+   while(<(count, 100),
+         do(define(total, +(total, count)),
+            define(count, +(count, 1)))),
+   print(total))
+`);
+
+module.exports = run;
